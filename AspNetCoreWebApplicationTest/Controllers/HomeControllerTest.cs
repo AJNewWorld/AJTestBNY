3efbd276412485f24dd8cb2e712efe72045e0ffb
Amit Jha <amitkjha@outlook.com>
2019-02-08 11:27:22 -0500

    Loft changes

--- AspNetCoreWebApplicationTest/Controllers/HomeControllerTest.cs
+++ AspNetCoreWebApplicationTest/Controllers/HomeControllerTest.cs
@@ -12,7 +12,7 @@ namespace AspNetCoreWebApplicationTest.Controllers
             HomeController controller = new HomeController();
             ViewResult result = (ViewResult) controller.Index();
             Assert.Single(result.ViewData);
-            Assert.Equal("TSYS just put Loft App on the cloud! YAAAY !!", result.ViewData["Message"]);
+            Assert.Equal("LOFT just put Loft App on the cloud! YAAAY !!", result.ViewData["Message"]);
         }
 
         [Fact]
